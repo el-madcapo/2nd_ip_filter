1fb6ea09e23179e4d6babf0c0202e9630731402c
testmepleases <40558876+testmepleases@users.noreply.github.com>
2018-07-14 14:26:29 +0400

    rebuild output function, it's more flexible now

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -2,8 +2,18 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cmath>
+#include <map>
 
+<<<<<<< HEAD
+
+#define ANY -1
+
+using vec_vec_int = std::vector<std::vector<int>>;
+
+bool isAny(int byte) // is ip byte equal any value [*](as 1.*.*.3)
+{
+	return byte == ANY;
+=======
 using vec_vec_int = std::vector<std::vector<int>>;
 
 void ip_sorting(const vec_vec_int& vector_original, vec_vec_int& vector_sorted, 
@@ -47,10 +57,25 @@ void ip_sorting(const vec_vec_int& vector_original, vec_vec_int& vector_sorted,
         	|| vector_sorted[vector_sorted.size() - 1][3] == 46)  
         	    { number_indexes_46.emplace_back(vector_sorted.size() - 1); }
     }
+>>>>>>> 2085cb26bc79f890eef3f6871b9995168fc4cb2d
 }
 
 
-std::vector<int> split(const std::string &str, char d, int times)
+bool isMatch(int byte, int value) // is byte value match our value
+{
+	return byte == value;
+}
+
+
+void ipSorting(vec_vec_int& vector_original)  // sorting by lexicographical_compare
+{                      
+	std::sort(vector_original.begin(),vector_original.end(),[](auto &i,auto &j) {
+      return std::lexicographical_compare(j.begin(),j.end(),i.begin(),i.end());
+    });
+}
+
+ 
+std::vector<int> split(const std::string &str, char d, int times) // function for extracting ip addresses from input stream
 {
     std::vector<int> r;
     int count = 0;
@@ -60,18 +85,111 @@ std::vector<int> split(const std::string &str, char d, int times)
     std::string::size_type stop = str.find_first_of(d);
 
     while(stop != std::string::npos || count == times || stop != space_index)
+<<<<<<< HEAD
+    {        
+        std::string ip_byte_in_string = str.substr(start, stop - start);
+        int ip_byte_number = (std::stoi(ip_byte_in_string));
+        r.emplace_back(ip_byte_number);
+=======
     {
         r.emplace_back(std::stoi(str.substr(start, stop - start)));
+>>>>>>> 2085cb26bc79f890eef3f6871b9995168fc4cb2d
         ++count;
         start = stop + 1;
         stop = str.find_first_of(d, start);
     }
-
-    r.emplace_back(std::stoi(str.substr(start)));
+    std::string str2 = str.substr(start);
+    int number = std::stoi(str2);
+    r.emplace_back(number);
 
     return r;
 }
 
+<<<<<<< HEAD
+
+void output_filter(const vec_vec_int& ip_pool, //function for output ip addresses
+	   int byte_1, int byte_2, 
+	   int byte_3, int byte_4) 
+{
+	std::map<int, int> matching_bytes;	
+	
+	bool is_byte1_any = isAny(byte_1);
+	if(!is_byte1_any) matching_bytes.insert(std::pair<int, int>(0, byte_1));
+	
+	bool is_byte2_any = isAny(byte_2);
+	if(!is_byte2_any) matching_bytes.insert(std::pair<int, int>(1, byte_2));
+	
+	bool is_byte3_any = isAny(byte_3);
+	if(!is_byte3_any) matching_bytes.insert(std::pair<int, int>(2, byte_3));
+
+	bool is_byte4_any = isAny(byte_4);
+	if(!is_byte4_any) matching_bytes.insert(std::pair<int, int>(3, byte_4));
+
+	int how_many_match = 0; // this variable for detecting of match condition 
+						//(if it greater than zero - all match, if it's equal zero  -  condition failed) 
+
+	for(auto& ip : ip_pool) 
+        {
+            if(!matching_bytes.empty()) { // if not all byte are *.*.*.* 
+
+            
+            	for(auto& i : matching_bytes) //section for matching our condition
+            	{
+            	
+	            	int byte_index = i.first;
+	            	int byte_value = i.second;
+	            	
+	            	bool is_byte_match = isMatch(ip[byte_index], byte_value);
+	            	
+	            	if(!is_byte_match) {
+	            		how_many_match = 0;
+	            		break;
+	            	}
+
+	            	++how_many_match;
+	            }
+
+            	if(how_many_match > 0)
+            	{
+            		std::cout << ip[0] << "."
+            			  	  << ip[1] << "."
+            			  	  << ip[2] << "."
+            			  	  << ip[2] << std::endl;
+            		how_many_match = 0;
+            	}
+            }
+            else {
+            	std::cout << ip[0] << "."
+            			  << ip[1] << "."
+            			  << ip[2] << "."
+            			  << ip[2] << std::endl;
+            }            
+        }                
+}
+
+
+void output_filter(vec_vec_int& ip_pool, int any_byte)
+{
+    for(auto& ip : ip_pool)
+    {
+    	bool is_byte1_match = isMatch(ip[0], any_byte);
+    	bool is_byte2_match = isMatch(ip[1], any_byte);
+    	bool is_byte3_match = isMatch(ip[2], any_byte);
+    	bool is_byte4_match = isMatch(ip[3], any_byte);
+    	if (is_byte1_match || is_byte2_match
+    		|| is_byte3_match || is_byte4_match)
+    	{
+    		    std::cout << ip[0] << "."
+            			  << ip[1] << "."
+            			  << ip[2] << "."
+            			  << ip[3] << std::endl;
+    	}
+    }
+}
+
+
+=======
+>>>>>>> 2085cb26bc79f890eef3f6871b9995168fc4cb2d
 int main()
 {
     try
@@ -82,8 +200,21 @@ int main()
         {
             auto v = split(line, '.', 4);
             ip_pool.emplace_back(v);
-        }     
+        }  
+        
+        ipSorting(ip_pool);
 
+<<<<<<< HEAD
+        output_filter(ip_pool, ANY, ANY, ANY, ANY);
+
+        output_filter(ip_pool, 1, ANY, ANY, ANY);
+
+        output_filter(ip_pool, 46, 70, ANY, ANY);
+
+        output_filter(ip_pool, 46);
+        
+        return 0;
+=======
         vec_vec_int ip_pool_sorted;
         std::vector<unsigned int> indexes_1;
         std::vector<unsigned int> indexes_4670;
@@ -124,6 +255,7 @@ int main()
     		          << ip_pool_sorted[indexes_46[i]][2] << "." 
     		          << ip_pool_sorted[indexes_46[i]][3] << std::endl;
         }     
+>>>>>>> 2085cb26bc79f890eef3f6871b9995168fc4cb2d
    }
     catch(const std::exception &e)
     {
