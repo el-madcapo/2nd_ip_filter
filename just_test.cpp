ce5782a06165a887bb81c59317db5b63c509fe3c
testmepleases <ylyagin@yandex.ru>
2018-08-04 21:48:44 +0400

    boost test was added

---
+++ just_test.cpp
@@ -0,0 +1,28 @@
+#include "ip_filter.cpp"
+#include <boost/test/unit_test.hpp>
+
+#define BOOST_TEST_MODULE TestIpFilter
+
+BOOST_AUTO_TEST_CASE( TestIpFilter )
+{
+       BOOST_CHECK( isIp4Adress("0.0.0.0	4	4") == true);
+      BOOST_CHECK( isIp4Adress("22.99.83.10	55	68") == true);
+      BOOST_CHECK( isIp4Adress("199.100.200.255	8	76") == true);
+      BOOST_CHECK( isIp4Adress("0.0.0.0	4	4") == true);
+      BOOST_CHECK( isIp4Adress("255.255.255.255	4	4") == true);
+      BOOST_CHECK( isIp4Adress("8.8.8.8") == true);
+      BOOST_CHECK( isIp4Adress("99.99.99.99 2 3") == true);
+}
+
+BOOST_AUTO_TEST_CASE(TestIpFilterNoncorrectIp)
+    {
+      BOOST_CHECK( isIp4Adress("255.256.255.255	4	4") == false);
+      BOOST_CHECK( isIp4Adress("256.255.255.255	4	4") == false);
+      BOOST_CHECK( isIp4Adress("255.255.300.255	4	4") == false);
+      BOOST_CHECK( isIp4Adress("255.255.255.400	4	4") == false);
+      BOOST_CHECK( isIp4Adress("1.-1.255.255	4	4") == false);
+      BOOST_CHECK( isIp4Adress("1.1.-1.3	4	4") == false);
+      BOOST_CHECK( isIp4Adress("1.1.255.-1	4	4") == false);
+    }
+
+
