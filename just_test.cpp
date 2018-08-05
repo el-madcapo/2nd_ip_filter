b61974c3514fffc36360913d85d7b158f935c31a
testmepleases <40558876+testmepleases@users.noreply.github.com>
2018-08-05 11:08:10 +0400

    added working unit test

--- just_test.cpp
+++ just_test.cpp
@@ -3,9 +3,11 @@
 
 #define BOOST_TEST_MODULE TestIpFilter
 
-BOOST_AUTO_TEST_CASE( TestIpFilter )
+BOOST_AUTO_TEST_SUITE(TesuIpFilter)
+
+BOOST_AUTO_TEST_CASE( TestIpFilterCorrect )
 {
-       BOOST_CHECK( isIp4Adress("0.0.0.0	4	4") == true);
+      BOOST_CHECK( isIp4Adress("0.0.0.0	4	4") == true);
       BOOST_CHECK( isIp4Adress("22.99.83.10	55	68") == true);
       BOOST_CHECK( isIp4Adress("199.100.200.255	8	76") == true);
       BOOST_CHECK( isIp4Adress("0.0.0.0	4	4") == true);
@@ -15,7 +17,7 @@ BOOST_AUTO_TEST_CASE( TestIpFilter )
 }
 
 BOOST_AUTO_TEST_CASE(TestIpFilterNoncorrectIp)
-    {
+{
       BOOST_CHECK( isIp4Adress("255.256.255.255	4	4") == false);
       BOOST_CHECK( isIp4Adress("256.255.255.255	4	4") == false);
       BOOST_CHECK( isIp4Adress("255.255.300.255	4	4") == false);
@@ -23,6 +25,6 @@ BOOST_AUTO_TEST_CASE(TestIpFilterNoncorrectIp)
       BOOST_CHECK( isIp4Adress("1.-1.255.255	4	4") == false);
       BOOST_CHECK( isIp4Adress("1.1.-1.3	4	4") == false);
       BOOST_CHECK( isIp4Adress("1.1.255.-1	4	4") == false);
-    }
+}
 
-
+BOOST_AUTO_TEST_SUITE_END()
